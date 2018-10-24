07911b209998bafceb104c67c16d4b9e34fb913a
Yurii Chornyi <chornyi.yurii@gmail.com>
2018-10-24 18:35:48 +0300

    initial commit and ng material initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewAdvertisementComponent } from './new-advertisement/new-advertisement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-advertisement', component: NewAdvertisementComponent }
+  // ,{ path: 'advertisement/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'advertisements',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule,MatCheckboxModule,MatToolbarModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule} from '@angular/material';
+import { NewAdvertisementComponent } from './new-advertisement/new-advertisement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewAdvertisementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
